168eece2a0b2820ec2b1aa86d8e50f3ab7536da2
patrikfernberg <p.fernberg@gmail.com>
2016-12-13 16:23:02 +0100

    Add files via upload

---
+++ register_script.php
@@ -0,0 +1,42 @@
+<?php
+	include("pdo_connect.php");
+	
+	$user = $_POST['username'];
+	
+	$userExist = False;
+	$userTable = $pdo->query("SELECT * FROM user");
+	while($userRow = $userTable->fetch(PDO::FETCH_ASSOC)){
+		if($userRow['username'] == $user){
+			$userExist = True;
+		}
+	}
+	
+	if(empty($_POST['username']) || empty($_POST['password'])){
+		echo 'FIELDS CANNOT BE EMPTY';
+		header("refresh:3; url=index.php");
+		exit;
+	}
+	elseif($userExist == False){
+		$register = "INSERT INTO user (username, password)
+		VALUES (:username, :password)";
+		
+		$stmt = $pdo->prepare($register);
+		$stmt->execute(
+			array(
+			':username' => $_POST['username'],
+			':password' => $_POST['password']
+			)
+		);
+		//echo 'REGISTRATION SUCCESSFUL';
+		header("refresh:0; url=home.php");
+		exit;
+	}
+	else{
+		echo 'USERNAME ALREADY EXISTS';
+		header("refresh:3; url=index.php");
+		exit;
+	}
+
+	$pdo = null;
+
+?>
