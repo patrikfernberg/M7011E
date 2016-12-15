a1947e0af7b1d1fffbe7d444381a78fd8449f981
patrikfernberg <p.fernberg@gmail.com>
2016-12-15 17:47:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <html>
 <head>
     <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
     <title>M7011E</title>
 
     <!-- Bootstrap CSS -->
